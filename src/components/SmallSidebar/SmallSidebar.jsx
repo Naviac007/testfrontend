019e03e55822b15b69a05181a885da2e4933c11e
Naviac007 <naviac007@gmail.com>
2022-01-14 13:04:09 +0530

    comit sat

--- src/components/SmallSidebar/SmallSidebar.jsx
+++ src/components/SmallSidebar/SmallSidebar.jsx
@@ -7,36 +7,91 @@ import subscriptions from "../../assets/images/subscriptions.svg";
 import trending from "../../assets/images/trending.svg";
 import Box from '@mui/material/Box';
 import "./small-sidebar.css";
-
+import ReactSession from "react-client-session/dist/ReactSession";
+import Snackbar from '@mui/material/Snackbar';
 export default class SmallSidebar extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			openSnack: false,
+
+		}
+	}
+	handleClick = () => {
+		this.setState({ openSnack: true });
+		console.log(this.state.openSnack)
+	};
+	handleClose = () => {
+		this.setState({ openSnack: false });
+	};
 	render() {
+		let loginNavigation;
+		if (ReactSession.get("username")) {
+			loginNavigation = <ul>
+				<li>
+					<Link to='/' className="new_video" style={{ display: "block" }}>
+						<i class="fas fa-home"></i>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link to='/' className="new_video" style={{ display: "block" }}>
+						<i class="fas fa-thumbs-up"></i>
+						Liked Videos
+					</Link>
+				</li>
+				<li>
+					<Link to='/' className="new_video" style={{ display: "block" }}>
+						<i class="fas fa-file-video"></i>
+						Library
+					</Link>
+				</li>
+				<li><Link to='/upload' className="new_video" style={{ display: "block" }}>
+					<i class="fas fa-plus-square"></i>
+					Add Video
+				</Link></li>
+			</ul>
+		}
+		else {
+			loginNavigation = <ul>
+				<li>
+					<Link to='/' className="new_video" style={{ display: "block" }}>
+						<i class="fas fa-home"></i>
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link onClick={this.handleClick} className="new_video" style={{ display: "block" }}>
+						<i class="fas fa-thumbs-up"></i>
+						Liked Videos
+					</Link>
+				</li>
+				<li>
+					<Link onClick={this.handleClick} className="new_video" style={{ display: "block" }}>
+						<i class="fas fa-file-video"></i>
+						Library
+					</Link>
+				</li>
+				<li><Link onClick={this.handleClick} className="new_video" style={{ display: "block" }}>
+					<i class="fas fa-plus-square"></i>
+					Add Video
+				</Link></li>
+			</ul>
+		}
+		const { openSnack } = this.state;
 		return (
 
 			<section class="sidebar_small" >
 				<Box position="fixed" sx={{ width: 76, bgcolor: 'white', display: { xs: 'none', md: 'flex' } }}>
-					<ul>
-						<li>
-							<Link to='/' className="new_video" style={{ display: "block" }}>
-								<i class="fas fa-home"></i>
-								Home
-							</Link>
-						</li>
-						<li>
-							<Link to='/' className="new_video" style={{ display: "block" }}>
-								<i class="fas fa-thumbs-up"></i>
-								Liked Videos
-							</Link>
-						</li>
-						<li>
-							<Link to='/' className="new_video" style={{ display: "block" }}>
-								<i class="fas fa-file-video"></i>
-								Library
-							</Link>
-						</li>
-						<li><Link to='/upload' className="new_video" style={{ display: "block" }}>
-							Add Video
-						</Link></li>
-					</ul>
+					{loginNavigation}
+					<Snackbar
+						open={openSnack}
+						autoHideDuration={3000}
+						onClose={this.handleClose}
+						message="Please Sign-In first"
+
+					/>
 				</Box>
 			</section >
 
--- src/components/TestHeader/Header.js
+++ src/components/TestHeader/Header.js
@@ -21,9 +21,10 @@ import { styled, alpha } from '@mui/material/styles';
 import Modal from '@mui/material/Modal';
 import { purple } from '@mui/material/colors';
 import { ReactSession } from 'react-client-session';
+import LogoutIcon from '@mui/icons-material/Logout';
 import history from '../History/History'
-import "./header.css";
-
+import "./headerStyle.css";
+import axios from 'axios';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -136,43 +137,101 @@ const onSearchChange=(event)=>{
   ];
 
   const errors = {
-    uname: "invalid username",
+    uname: "invalid Email",
     pass: "invalid password"
   };
 const handleSearch = ()=>{
   var string = document.getElementById("search").value
   console.log(string)
-  history.push('/Search/'+string);
-  window.location.reload();
+  if(string!=""){
+    history.push('/Search/'+string);
+    window.location.reload();
+  }
+  
 }
   const handleSubmit = (event) => {
     //Prevent page reload
     event.preventDefault();
-
-
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-    
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-        ReactSession.set("username", "Bob");
-				ReactSession.set("isSubmitted", true);
-				setLogin(false);
-				window.location.reload();
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
+    var email=/^([a-zA-Z0-9\._-]+)@([a-zA-Z-]+).([a-z]{2,8})(\.[a-z]{2,8})?(\.[a-z]{2,8})?$/;
+    var { uname, pass } = document.forms["sign-in-form"];
+    if (uname.value.trim() == "") {
+      
+      setErrorMessages({ name: "uname", message: "cannot be empty" });
+     return
     }
+    else if(!email.test(uname.value)){
+      setErrorMessages({ name: "uname", message: "not valid email" });
+     return
+    }
+    if (pass.value.trim() == "") {
+      
+      setErrorMessages({ name: "pass", message: "cannot be empty" });
+     return
+    }
+    else if(pass.value.length<=5){
+      setErrorMessages({ name: "pass", message: "password lenght cannot be less than 5" });
+     return
+    }
+   
+    // Find user login info
+    //const userData = database.find((user) => user.username === uname.value);
+    const res = axios({
+      method: 'post',
+      url: 'http://localhost:31772/api/Users/login',
+      data: {
+        "email": uname.value,
+        "password": pass.value
+      },
+      config: {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }
+    })
+    console.log("outside then")
+    res.then((response) => {
+      console.log("in then")
+      if (response.status == 200) {
+        console.log(response);
+        console.log(response.headers)
+        console.log(response.headers["uname"])
+        ReactSession.set("username", response.headers["uname"]);
+        ReactSession.set("isSubmitted", true);
+        ReactSession.set("token", response.headers["jwt"])
+        ReactSession.set("userId", response.headers["uid"])
+        setLogin(false)
+        window.location.reload();
+      }
+      if(response.status == 400){
+        console.log("here")
+        setErrorMessages({ name: "name", message: "invalid username or pass" });
+        
+      }
+    }).catch((response) => {
+      console.log("here")
+      console.log(response.status);
+      setErrorMessages({ name: "uname", message: "invalid username or pass" });
+      console.log(errorMessages)
+        
+    });
+    // Compare user info
+    // if (userData) {
+    
+    //   if (userData.password !== pass.value) {
+    //     // Invalid password
+    //     setErrorMessages({ name: "pass", message: errors.pass });
+    //   } else {
+    //     setIsSubmitted(true);
+    //     ReactSession.set("username", "Bob");
+		// 		ReactSession.set("isSubmitted", true);
+		// 		setLogin(false);
+		// 		window.location.reload();
+    //   }
+    // } else {
+    //   // Username not found
+    //   setErrorMessages({ name: "uname", message: errors.uname });
+    // }
   };
 
 
@@ -187,7 +246,7 @@ const handleSearch = ()=>{
 
   let button;
   if (ReactSession.get("isSubmitted")) {
-      button = <Button sx={{color: 'white', display: 'block' }} className=""  onClick={logOut}>
+      button = <Button sx={{color: 'white', display: 'block' }} className=""  onClick={logOut} startIcon={<LogoutIcon />}>
  
           Logout
       </Button>
@@ -201,14 +260,15 @@ const handleSearch = ()=>{
 
   return (
     <>
-    <AppBar position="fixed">
+    <AppBar position="fixed" sx={{backgroundColor:'black'}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 0.1,mr: 2, display: { xs: 'none', md: 'flex' } }}
+            onClick={()=>{history.push("/");window.location.reload();}}
+            sx={{ flexGrow: 0.1,mr: 2, display: { xs: 'none', md: 'flex' },cursor:"pointer" }}
           >
             <img src={logo} style={{maxHeight : "30px",maxWidth : "130px"}}/>
           </Typography>
@@ -241,12 +301,35 @@ const handleSearch = ()=>{
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: 'flex', md: 'none' },width: "80%"
               }}
             >
-               <Link to='/upload' className="new_video" style={{display:"block"}}>						
-						Add Video
-			</Link>
+              {/////------------------------menu for small size-----------------------------------}
+}
+            <ul style={{width:"100%"}}>
+						<li className='nav-link-small' style={{padding: "10px"}}>
+							<Link to='/'  style={{ display: "flex",    alignItems: "center"}}>
+								<i class="fas fa-home"></i>
+								Home
+							</Link>
+						</li>
+						<li className='nav-link-small'>
+							<Link to='/' style={{ display: "block" }}>
+								<i class="fas fa-thumbs-up"></i>
+								Liked Videos
+							</Link>
+						</li>
+						<li className='nav-link-small'>
+							<Link to='/'  style={{ display: "block" }}>
+								<i class="fas fa-file-video"></i>
+								Library
+							</Link>
+						</li>
+						<li className='nav-link-small'><Link to='/upload'  style={{ display: "block" }}>
+            <i class="fas fa-plus-square"></i>
+              Add Video
+						</Link></li>
+					</ul>
             </Menu>
           </Box>
           
@@ -304,7 +387,7 @@ const handleSearch = ()=>{
     <div className="modal-body">
       <h2>Login</h2>
       <span className="subtitle">Just fill in the form below</span>
-      <form className="contact-form form-validate4" novalidate="novalidate" onSubmit={handleSubmit}>
+      <form className="contact-form form-validate4" name='sign-in-form' novalidate="novalidate" onSubmit={handleSubmit}>
         <div className="form-group">
           <label for="username"> Username</label>
           <input className="form-control" type="email" name="uname" placeholder="E-mail" required="" autocomplete="off" aria-required="true" />
